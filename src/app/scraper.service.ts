a437cd9c26acc0791a71d6db314c475e848d1d6a
AbdelfattahToulaoui <aft.power.2014@gmail.com>
2020-04-06 00:01:24 +0100

    Implemented methods for service

--- src/app/scraper.service.ts
+++ src/app/scraper.service.ts
@@ -1,26 +1,45 @@
 import { Injectable } from '@angular/core';
+import { HttpClientModule,HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ScraperService {
 
-  constructor() { }
-  private _data = null;
+  constructor(private http: HttpClient) { }
+  private _data: Array<Object> = null;
+  public fetching: boolean = false;
+  private url: string = 'https://coronavirus-19-api.herokuapp.com/countries';
 
-  get data(){
-    if(! this._data ){
-
+  public get data(){
+    if(! this._data && !this.fetching){
+      this.refresh();
     }
-    return !this._data;
+    return this._data;
   }
 
-  public refresh(){
-
+  public refresh(country:string = null){
+    this.fetching = true;
+    this.http.get(this.url)
+          .subscribe((response: Array<Object>)=> {
+            this._data = response;
+            this.fetching = false;
+        });
   }
 
-  get countries(){
+  public get countries(){
+    return this.data.map(
+      (obj) => {
+        return obj['country'];
+      }
+    );
+  }
 
-    return null;
+  public countryData(country='World'){
+    return this.data.find(
+      (obj) => {
+        return obj['country']==country;
+      }
+    );
   }
 }
